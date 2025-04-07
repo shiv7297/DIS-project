b137d6d90680a2c8c7aeb4734b80d02008b21db1
shiv7297 <2023ucp1655@mnit.ac.in>
2025-04-07 20:17:07 +0000

    separate css files

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+
